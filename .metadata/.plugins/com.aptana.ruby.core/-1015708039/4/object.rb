64b36d58d7e27432c828f866b7db355c76c40202
bryantt23 <bryantt23@gmail.com>
2015-03-14 22:47:34 -0700

    Initial Commit

---
+++ .metadata/.plugins/com.aptana.ruby.core/-1015708039/4/object.rb
@@ -0,0 +1,193 @@
+class Object < BasicObject
+  include Kernel
+
+  ARGF = ARGF
+  ARGV = []
+  Addrinfo = Addrinfo
+  ArgumentError = ArgumentError
+  Array = Array
+  BasicObject = BasicObject
+  BasicSocket = BasicSocket
+  Bignum = Bignum
+  Binding = Binding
+  CGI = CGI
+  CROSS_COMPILING = nil
+  Class = Class
+  Comparable = Comparable
+  Complex = Complex
+  ConditionVariable = Thread::ConditionVariable
+  Data = Data
+  Date = Date
+  DateTime = DateTime
+  Delegator = Delegator
+  Digest = Digest
+  Dir = Dir
+  ENV = {"XDG_SESSION_PATH"=>"/org/freedesktop/DisplayManager/Session0", "rvm_version"=>"1.26.10 (latest)", "UPSTART_SESSION"=>"unix:abstract=/com/ubuntu/upstart-session/1000/1179", "SESSION_MANAGER"=>"local/bryantt-Satellite-L745:@/tmp/.ICE-unix/1409,unix/bryantt-Satellite-L745:/tmp/.ICE-unix/1409", "GNOME_DESKTOP_SESSION_ID"=>"this-is-deprecated", "GDMSESSION"=>"ubuntu", "IM_CONFIG_PHASE"=>"1", "COMPIZ_CONFIG_PROFILE"=>"ubuntu", "MANDATORY_PATH"=>"/usr/share/gconf/ubuntu.mandatory.path", "PWD"=>"/home/bryantt/Downloads/Aptana_Studio_3", "SESSIONTYPE"=>"gnome-session", "GIO_LAUNCHED_DESKTOP_FILE_PID"=>"3812", "GTK_IM_MODULE"=>"ibus", "XDG_GREETER_DATA_DIR"=>"/var/lib/lightdm-data/bryantt", "MY_RUBY_HOME"=>"/home/bryantt/.rvm/rubies/ruby-2.2.0", "XDG_MENU_PREFIX"=>"gnome-", "TEXTDOMAIN"=>"im-config", "SELINUX_INIT"=>"YES", "_system_type"=>"Linux", "XDG_CONFIG_DIRS"=>"/etc/xdg/xdg-ubuntu:/usr/share/upstart/xdg:/etc/xdg", "XDG_CURRENT_DESKTOP"=>"Unity", "PATH"=>"/home/bryantt/.rvm/gems/ruby-2.2.0/bin:/home/bryantt/.rvm/gems/ruby-2.2.0@global/bin:/home/bryantt/.rvm/rubies/ruby-2.2.0/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/home/bryantt/.rvm/bin:/home/bryantt/.rvm/bin", "APTANA_VERSION"=>"3.4.2.1368863613", "rvm_path"=>"/home/bryantt/.rvm", "XAUTHORITY"=>"/home/bryantt/.Xauthority", "GDM_LANG"=>"en_US", "_system_version"=>"14.04", "rvm_stored_umask"=>"0002", "XDG_SEAT"=>"seat0", "XDG_SESSION_ID"=>"c1", "XDG_SEAT_PATH"=>"/org/freedesktop/DisplayManager/Seat0", "XDG_VTNR"=>"7", "GEM_PATH"=>"/home/bryantt/.rvm/gems/ruby-2.2.0:/home/bryantt/.rvm/gems/ruby-2.2.0@global", "GNOME_KEYRING_CONTROL"=>"/run/user/1000/keyring-PgecKL", "GTK_MODULES"=>"overlay-scrollbar:unity-gtk-module", "SHLVL"=>"1", "_system_arch"=>"x86_64", "_system_name"=>"Ubuntu", "TEXTDOMAINDIR"=>"/usr/share/locale/", "COMPIZ_BIN_PATH"=>"/usr/bin/", "JOB"=>"dbus", "XDG_DATA_DIRS"=>"/usr/share/ubuntu:/usr/share/gnome:/usr/local/share/:/usr/share/", "QT_IM_MODULE"=>"ibus", "QT_QPA_PLATFORMTHEME"=>"appmenu-qt5", "GIO_LAUNCHED_DESKTOP_FILE"=>"/home/bryantt/.local/share/applications/aptana_studio_3.desktop", "rvm_prefix"=>"/home/bryantt", "LOGNAME"=>"bryantt", "GPG_AGENT_INFO"=>"/tmp/gpg-WYYtRm/S.gpg-agent:1384:1", "IRBRC"=>"/home/bryantt/.rvm/rubies/ruby-2.2.0/.irbrc", "SESSION"=>"ubuntu", "XMODIFIERS"=>"@im=ibus", "RUBY_VERSION"=>"ruby-2.2.0", "SSH_AUTH_SOCK"=>"/run/user/1000/keyring-PgecKL/ssh", "SHELL"=>"/bin/bash", "DBUS_SESSION_BUS_ADDRESS"=>"unix:abstract=/tmp/dbus-rBWmT1YIAm", "rvm_bin_path"=>"/home/bryantt/.rvm/bin", "GNOME_KEYRING_PID"=>"", "LANGUAGE"=>"en_US", "GEM_HOME"=>"/home/bryantt/.rvm/gems/ruby-2.2.0", "INSTANCE"=>"", "DESKTOP_SESSION"=>"ubuntu", "CLUTTER_IM_MODULE"=>"xim", "DISPLAY"=>":0", "USER"=>"bryantt", "HOME"=>"/home/bryantt", "rvm_loaded_flag"=>"1", "QT4_IM_MODULE"=>"xim", "DEFAULTS_PATH"=>"/usr/share/gconf/ubuntu.default.path", "rvm_user_install_flag"=>"1", "XDG_RUNTIME_DIR"=>"/run/user/1000", "LANG"=>"en_US.UTF-8"}
+  EOFError = EOFError
+  Encoding = Encoding
+  EncodingError = EncodingError
+  Enumerable = Enumerable
+  Enumerator = Enumerator
+  Errno = Errno
+  Etc = Etc
+  Exception = Exception
+  FALSE = false
+  FalseClass = FalseClass
+  Fcntl = Fcntl
+  Fiber = Fiber
+  FiberError = FiberError
+  File = File
+  FileTest = FileTest
+  FileUtils = FileUtils
+  Fixnum = Fixnum
+  Float = Float
+  FloatDomainError = FloatDomainError
+  GC = GC
+  Gem = Gem
+  Hash = Hash
+  IO = IO
+  IOError = IOError
+  IPSocket = IPSocket
+  IndexError = IndexError
+  Integer = Integer
+  Interrupt = Interrupt
+  Kernel = Kernel
+  KeyError = KeyError
+  LoadError = LoadError
+  LocalJumpError = LocalJumpError
+  Marshal = Marshal
+  MatchData = MatchData
+  Math = Math
+  Method = Method
+  Module = Module
+  Monitor = Monitor
+  MonitorMixin = MonitorMixin
+  Mutex = Mutex
+  NIL = nil
+  NameError = NameError
+  Net = Net
+  NilClass = NilClass
+  NoMemoryError = NoMemoryError
+  NoMethodError = NoMethodError
+  NotImplementedError = NotImplementedError
+  Numeric = Numeric
+  OUTPUT_PATH = "/home/bryantt/GitHub/.metadata/.plugins/com.aptana.ruby.core/-1015708039/4/"
+  Object = Object
+  ObjectSpace = ObjectSpace
+  OpenSSL = OpenSSL
+  OptParse = OptionParser
+  OptionParser = OptionParser
+  Proc = Proc
+  Process = Process
+  Queue = Thread::Queue
+  RUBY_COPYRIGHT = "ruby - Copyright (C) 1993-2014 Yukihiro Matsumoto"
+  RUBY_DESCRIPTION = "ruby 2.2.0p0 (2014-12-25 revision 49005) [x86_64-linux]"
+  RUBY_ENGINE = "ruby"
+  RUBY_PATCHLEVEL = 0
+  RUBY_PLATFORM = "x86_64-linux"
+  RUBY_RELEASE_DATE = "2014-12-25"
+  RUBY_REVISION = 49005
+  RUBY_VERSION = "2.2.0"
+  Random = Random
+  Range = Range
+  RangeError = RangeError
+  Rational = Rational
+  RbConfig = RbConfig
+  Regexp = Regexp
+  RegexpError = RegexpError
+  Resolv = Resolv
+  RubyVM = RubyVM
+  RuntimeError = RuntimeError
+  STDERR = IO.new
+  STDIN = IO.new
+  STDOUT = IO.new
+  ScanError = StringScanner::Error
+  ScriptError = ScriptError
+  SecureRandom = SecureRandom
+  SecurityError = SecurityError
+  Signal = Signal
+  SignalException = SignalException
+  SimpleDelegator = SimpleDelegator
+  SizedQueue = Thread::SizedQueue
+  Socket = Socket
+  SocketError = SocketError
+  StandardError = StandardError
+  StopIteration = StopIteration
+  String = String
+  StringIO = StringIO
+  StringScanner = StringScanner
+  Struct = Struct
+  Symbol = Symbol
+  SyntaxError = SyntaxError
+  SystemCallError = SystemCallError
+  SystemExit = SystemExit
+  SystemStackError = SystemStackError
+  TCPServer = TCPServer
+  TCPSocket = TCPSocket
+  TOPLEVEL_BINDING = #<Binding:0x00000001277288>
+  TRUE = true
+  TSort = TSort
+  Tempfile = Tempfile
+  Thread = Thread
+  ThreadError = ThreadError
+  ThreadGroup = ThreadGroup
+  Time = Time
+  Timeout = Timeout
+  TimeoutError = Timeout::Error
+  TracePoint = TracePoint
+  TrueClass = TrueClass
+  TypeError = TypeError
+  UDPSocket = UDPSocket
+  UNIXServer = UNIXServer
+  UNIXSocket = UNIXSocket
+  URI = URI
+  UnboundMethod = UnboundMethod
+  UncaughtThrowError = UncaughtThrowError
+  ZeroDivisionError = ZeroDivisionError
+  Zlib = Zlib
+  fatal = nil
+
+
+
+  protected
+
+
+  private
+
+  def DelegateClass(arg0)
+  end
+
+  def Digest(arg0)
+  end
+
+  def dir_names(arg0)
+  end
+
+  def file_name(arg0)
+  end
+
+  def get_classes
+  end
+
+  def grab_instance_method(arg0, arg1)
+  end
+
+  def print_args(arg0)
+  end
+
+  def print_instance_method(arg0, arg1)
+  end
+
+  def print_method(arg0, arg1, arg2, arg3, arg4, *rest)
+  end
+
+  def print_type(arg0)
+  end
+
+  def print_value(arg0)
+  end
+
+  def timeout(arg0, arg1, arg2, *rest)
+  end
+
+end
